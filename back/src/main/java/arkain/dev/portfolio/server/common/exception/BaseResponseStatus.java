b4314b77064e33e909afceadbc16b716c890580a
mango <mango.koo@goorm.io>
2025-03-05 19:16:04 +0900

    docs: update comments to English

--- back/src/main/java/arkain/dev/portfolio/server/common/exception/BaseResponseStatus.java
+++ back/src/main/java/arkain/dev/portfolio/server/common/exception/BaseResponseStatus.java
@@ -7,9 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum BaseResponseStatus {
-  SUCCESS(HttpStatus.CREATED, "요청에 성공하였습니다."),
-  NO_PAGE(HttpStatus.BAD_REQUEST, "존재하지 않는 페이지 입니다"),
-  NO_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 유저 입니다"),
+  SUCCESS(HttpStatus.CREATED, "Request success"),
+  NO_USER(HttpStatus.BAD_REQUEST, "User not found"),
   ;
 
   private final HttpStatus httpStatus;
--- back/src/main/java/arkain/dev/portfolio/server/common/exception/GlobalExceptionHandler.java
+++ back/src/main/java/arkain/dev/portfolio/server/common/exception/GlobalExceptionHandler.java
@@ -17,58 +17,58 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  // Convertor 에서 바인딩 실패시 발생하는 예외
+  // Exception thrown when binding fails in the converter
   @ExceptionHandler(value = {HttpMessageNotReadableException.class})
   public ResponseDto<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
     log.error(
-        "handleHttpMessageNotReadableException() in GlobalExceptionHandler throw HttpMessageNotReadableException : {}",
-        e.getMessage());
+            "handleHttpMessageNotReadableException() in GlobalExceptionHandler threw HttpMessageNotReadableException: {}",
+            e.getMessage());
     return ResponseDto.fail(new CommonException(ErrorCode.BAD_REQUEST_JSON));
   }
 
-  // 지원되지 않는 HTTP 메소드를 사용할 때 발생하는 예외
+  // Exception thrown when an unsupported HTTP method is used
   @ExceptionHandler(
-      value = {NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
+          value = {NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
   public ResponseDto<?> handleNoPageFoundException(Exception e) {
     log.error(
-        "handleNoPageFoundException() in GlobalExceptionHandler throw NoHandlerFoundException : {}",
-        e.getMessage());
+            "handleNoPageFoundException() in GlobalExceptionHandler threw NoHandlerFoundException: {}",
+            e.getMessage());
     return ResponseDto.fail(new CommonException(ErrorCode.NOT_FOUND_END_POINT));
   }
 
-  // 메소드의 인자 타입이 일치하지 않을 때 발생하는 예외
+  // Exception thrown when the method argument type does not match
   @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
   public ResponseDto<?> handleArgumentNotValidException(MethodArgumentTypeMismatchException e) {
     log.error(
-        "handleArgumentNotValidException() in GlobalExceptionHandler throw MethodArgumentTypeMismatchException : {}",
-        e.getMessage());
+            "handleArgumentNotValidException() in GlobalExceptionHandler threw MethodArgumentTypeMismatchException: {}",
+            e.getMessage());
     return ResponseDto.fail(e);
   }
 
-  // 필수 파라미터가 누락되었을 때 발생하는 예외
+  // Exception thrown when a required parameter is missing
   @ExceptionHandler(value = {MissingServletRequestParameterException.class})
   public ResponseDto<?> handleArgumentNotValidException(MissingServletRequestParameterException e) {
     log.error(
-        "handleArgumentNotValidException() in GlobalExceptionHandler throw MethodArgumentNotValidException : {}",
-        e.getMessage());
+            "handleArgumentNotValidException() in GlobalExceptionHandler threw MethodArgumentNotValidException: {}",
+            e.getMessage());
     return ResponseDto.fail(e);
   }
 
-  // 개발자가 직접 정의한 예외
+  // Exception defined by the developer
   @ExceptionHandler(value = {CommonException.class})
   @ResponseBody
   public ResponseDto<?> handleApiException(CommonException e, HttpServletResponse response) {
     log.error(
-        "handleApiException() in GlobalExceptionHandler throw CommonException : {}",
-        e.getMessage());
+            "handleApiException() in GlobalExceptionHandler threw CommonException: {}",
+            e.getMessage());
     response.setStatus(e.getErrorCode().getHttpStatus().value());
     return ResponseDto.fail(e);
   }
 
-  // 서버, DB 예외
+  // Server and database-related exceptions
   @ExceptionHandler(value = {Exception.class})
   public ResponseDto<?> handleException(Exception e, HttpServletResponse response) {
-    log.error("handleException() in GlobalExceptionHandler throw Exception : {}", e.getMessage());
+    log.error("handleException() in GlobalExceptionHandler threw Exception: {}", e.getMessage());
     e.printStackTrace();
     response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
     return ResponseDto.fail(new CommonException(ErrorCode.INTERNAL_SERVER_ERROR));
