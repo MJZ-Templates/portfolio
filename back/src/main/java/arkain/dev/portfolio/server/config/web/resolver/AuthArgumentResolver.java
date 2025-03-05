379d5eb80fd455438f8bb43fd61fe7b39df74f8e
mango <mango.koo@goorm.io>
2025-03-05 19:04:45 +0900

    feat: add auth argument resolver
    argument resolver for get security context holder.

---
+++ back/src/main/java/arkain/dev/portfolio/server/config/web/resolver/AuthArgumentResolver.java
@@ -0,0 +1,29 @@
+package arkain.dev.portfolio.server.config.web.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Auth.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            return authentication.getPrincipal();
+        }
+        return null;
+    }
+}
