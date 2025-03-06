698b57586eafeb02b6da19bca5893b5b36831c62
Zuly (권정) <zuly.kwon@goorm.io>
2025-03-06 11:14:01 +0900

    fix: main page path change

--- front/src/pages/page.tsx
+++ front/src/pages/page.tsx
@@ -4,7 +4,7 @@
 import Layout from "@/app/layout";
 import Hero from "@/components/home/Hero";
 import About from "@/components/home/About";
-import Projects from "@/app/projects/page";
+import Projects from "@/components/home/Projects";
 import Skills from "@/components/home/Skills";
 import Contact from "@/components/home/Contact";
 
