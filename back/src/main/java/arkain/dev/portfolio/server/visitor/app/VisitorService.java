e9959af4ec1cbcfee198f59b2a398e83c6eff4fc
mango <mango.koo@goorm.io>
2025-03-10 11:04:28 +0900

    feat: hourly stats get api

--- back/src/main/java/arkain/dev/portfolio/server/visitor/app/VisitorService.java
+++ back/src/main/java/arkain/dev/portfolio/server/visitor/app/VisitorService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -28,11 +29,26 @@ public class VisitorService {
     public Map<Integer, Long> getHourlyStats() {
         List<Visitor> visitors = findAll();
 
-        return visitors.stream()
-                .collect(Collectors.groupingBy(visitor -> visitor.getLocalDateTime().getHour(),
+        // 오늘 날짜 가져오기
+        LocalDate today = LocalDate.now();
+
+        // 오늘 날짜에 해당하는 방문자만 필터링
+        Map<Integer, Long> hourlyStats = visitors.stream()
+                .filter(visitor -> visitor.getLocalDateTime().toLocalDate().equals(today)) // 오늘 날짜 필터링
+                .collect(Collectors.groupingBy(
+                        visitor -> visitor.getLocalDateTime().getHour(),
                         TreeMap::new,
-                        Collectors.counting()));
+                        Collectors.counting()
+                ));
+
+        // 0~23시까지 데이터가 없으면 0L로 채움
+        for (int i = 0; i < 24; i++) {
+            hourlyStats.putIfAbsent(i, 0L);
+        }
+
+        return hourlyStats;
     }
+
 
     @Transactional
     public CommonSuccess save(VisitorDto dto) {
