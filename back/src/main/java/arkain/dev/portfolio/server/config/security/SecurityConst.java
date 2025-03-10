2f5b391e77d1d97664d5dd09da0f00a3bfd716cf
mango <mango.koo@goorm.io>
2025-03-10 13:33:09 +0900

    feat: allow POST requests to /api/contact

--- back/src/main/java/arkain/dev/portfolio/server/config/security/SecurityConst.java
+++ back/src/main/java/arkain/dev/portfolio/server/config/security/SecurityConst.java
@@ -5,7 +5,9 @@ import java.util.List;
 public class SecurityConst {
 
     public static final List<String> ALLOWED_POST_APIS = List.of(
-            "/api/auth/login"
+            "/api/auth/login",
+            "/api/contact",
+            "/api/visitor"
     );
 
     private SecurityConst() {
